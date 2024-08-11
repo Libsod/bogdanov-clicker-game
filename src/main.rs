5c3d41e511744aebe3d5674ab352130013f7593b
bogdanov <bbogdan_ov@mail.ru>
2024-08-11 14:06:34 +0000

    feat: remove all the junk code and add something more worthy

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,82 @@
 use macroquad::prelude::*;
+use miniquad::window::screen_size;
 
-struct Size {
-    font: u16,
-    photo: Vec2,
+const TITLE: &str = "BOGDANOV.NET";
+const TITLE_FONT_SIZE: u16 = 64;
+
+/// Sprite
+#[derive(Debug)]
+struct Sprite<'a> {
+    texture: &'a Texture2D,
+    width: f32,
+    height: f32,
+    anchor: Vec2
 }
-
-struct Bogdanov<'a> {
-    text: &'a str,
-    photo: Texture2D,
-    font: Font,
-    size: Size,
-}
-
-impl<'a> Bogdanov<'a> {
-    async fn new(
-        text: &'a str,
-        photo: Texture2D,
-        font: Font,
-        size: Size,
-    ) -> Result<Bogdanov<'a>, macroquad::Error> {
-        Ok(Bogdanov {
-            text,
-            photo,
-            font,
-            size,
-        })
+impl<'a> Sprite<'a> {
+    pub fn new(texture: &'a Texture2D) -> Self {
+        Self {
+            texture,
+            width: texture.width(),
+            height: texture.height(),
+            anchor: vec2(0.0, 0.0)
+        }
     }
 
-    async fn default() -> Result<Bogdanov<'a>, macroquad::Error> {
-        Ok(Bogdanov {
-            text: "BOGDANOV.NET",
-            photo: load_texture("./assets/bogdanov.png").await?,
-            font: load_ttf_font("./assets/fonts/Minecraft-font.ttf").await?,
-            size: Size {
-                font: 8 * 8,
-                photo: vec2(450., 450.),
-            },
-        })
+    pub fn with_size(mut self, width: f32, height: f32) -> Self {
+        self.width = width;
+        self.height = height;
+        self
+    }
+    pub fn with_anchor(mut self, anchor: Vec2) -> Self {
+        self.anchor = anchor;
+        self
+    }
+
+    pub fn draw(&self, x: f32, y: f32) {
+        draw_texture_ex(
+            &self.texture,
+            x - self.width * self.anchor.x,
+            y - self.height * self.anchor.y,
+            WHITE,
+            DrawTextureParams {
+                dest_size: Some(vec2(self.width, self.height)),
+                ..Default::default()
+            }
+        );
     }
 }
 
 #[macroquad::main("BOGDANOV.NET")]
 async fn main() -> Result<(), macroquad::Error> {
-    let bogdanov = Bogdanov::default().await?;
+    let bogdanov_tex = load_texture("./assets/bogdanov.png").await?;
+    let title_font = load_ttf_font("./assets/fonts/Minecraft-font.ttf").await?;
 
-    let text_size = measure_text(bogdanov.text, Some(&bogdanov.font), bogdanov.size.font, 1.);
+    let bogdanov_sprite = Sprite::new(&bogdanov_tex)
+        .with_size(400.0, 400.0)
+        .with_anchor(vec2(0.5, 0.5));
+
+    let title_size = measure_text(TITLE, Some(&title_font), TITLE_FONT_SIZE, 1.0);
 
     loop {
         clear_background(WHITE);
 
-        draw_text_ex(
-            bogdanov.text,
-            (screen_width() / 2. - text_size.width / 2.).round(),
-            (text_size.offset_y + 35.).round(),
-            TextParams {
-                font: Some(&bogdanov.font),
-                font_size: bogdanov.size.font,
-                color: BLACK,
-                ..Default::default()
-            },
+        let (sw, sh) = screen_size();
+        let center_x = sw / 2.0;
+        let center_y = sh / 2.0;
+
+        bogdanov_sprite.draw(
+            center_x,
+            center_y,
         );
 
-        draw_texture_ex(
-            &bogdanov.photo,
-            (screen_width() / 2. - bogdanov.size.photo.x / 2.).round(),
-            (text_size.offset_y + 40.).round(),
-            WHITE,
-            DrawTextureParams {
-                dest_size: Some(bogdanov.size.photo),
+        draw_text_ex(
+            TITLE,
+            center_x - title_size.width / 2.0,
+            title_size.offset_y + 20.0,
+            TextParams {
+                font: Some(&title_font),
+                font_size: TITLE_FONT_SIZE,
+                color: BLACK,
                 ..Default::default()
             },
         );
