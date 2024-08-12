08c50ec15bb12e83d2481aae73fda5011f66ba67
bogdanov <bbogdan_ov@mail.ru>
2024-08-12 15:17:51 +0000

    feat: make bogdanov bounce off the screen edges

--- src/main.rs
+++ src/main.rs
@@ -49,35 +49,83 @@ impl<'a> Sprite<'a> {
 async fn main() -> Result<(), macroquad::Error> {
     const TITLE: &str = "BOGDANOV.NET";
     const TITLE_FONT_SIZE: u16 = 64;
+    const BOGDANOV_SPEED: f32 = 4.0;
+
+    let mut points: usize = 0;
 
     let bogdanov_texture = load_texture("./assets/bogdanov.png").await?;
     let title_font = load_ttf_font("./assets/fonts/Minecraft-font.ttf").await?;
 
+    let mut bogdanov_dir = vec2(1.0, 1.0);
+    let mut bogdanov_rect = Rect::new(0.0, 0.0, 200.0, 200.0);
     let bogdanov_sprite = Sprite::new(&bogdanov_texture)
-        .with_size(400.0, 400.0)
-        .with_anchor(vec2(0.5, 0.5));
+        .with_size(bogdanov_rect.w, bogdanov_rect.h);
 
     let title_size = measure_text(TITLE, Some(&title_font), TITLE_FONT_SIZE, 1.0);
+    let title_text_params = TextParams {
+        font: Some(&title_font),
+        font_size: TITLE_FONT_SIZE,
+        color: BLACK,
+        ..Default::default()
+    };
 
     loop {
+        let time = get_time() as f32;
+        let (screen_w, screen_h) = screen_size();
+        let mouse_pos: Vec2 = mouse_position().into();
+
+        // Moving bogdanov
+        bogdanov_rect.x += bogdanov_dir.x * BOGDANOV_SPEED;
+        bogdanov_rect.y += bogdanov_dir.y * BOGDANOV_SPEED;
+
+        // Bounce bogdanov off the screen edges
+        if bogdanov_rect.left() <= 0.0 {
+            bogdanov_rect.x = 0.0;
+            bogdanov_dir.x *= -1.0;
+        }
+        if bogdanov_rect.right() > screen_w {
+            bogdanov_rect.x = screen_w - bogdanov_rect.w;
+            bogdanov_dir.x *= -1.0;
+        }
+        if bogdanov_rect.top() <= 0.0 {
+            bogdanov_rect.y = 0.0;
+            bogdanov_dir.y *= -1.0;
+        }
+        if bogdanov_rect.bottom() > screen_h {
+            bogdanov_rect.y = screen_h - bogdanov_rect.h;
+            bogdanov_dir.y *= -1.0;
+        }
+
+        // Increase points if click on bogdanov
+        if bogdanov_rect.contains(mouse_pos) && is_mouse_button_pressed(MouseButton::Left) {
+            points += 1;
+        }
+
+        // Drawing
         clear_background(WHITE);
 
-        let (sw, sh) = screen_size();
-        let center_x = sw / 2.0;
-        let center_y = sh / 2.0;
+        let center_x = screen_w / 2.0;
+        let center_y = screen_h / 2.0;
 
-        bogdanov_sprite.draw(center_x, center_y);
+        bogdanov_sprite.draw(
+            bogdanov_rect.x,
+            bogdanov_rect.y,
+        );
 
         draw_text_ex(
             TITLE,
             center_x - title_size.width / 2.0,
             title_size.offset_y + 20.0,
-            TextParams {
-                font: Some(&title_font),
-                font_size: TITLE_FONT_SIZE,
-                color: BLACK,
-                ..Default::default()
-            },
+            title_text_params.clone(),
+        );
+
+        let points_str = points.to_string();
+        let points_size = measure_text(&points_str, Some(&title_font), TITLE_FONT_SIZE, 1.0);
+        draw_text_ex(
+            &points_str,
+            center_x - points_size.width / 2.0,
+            title_size.offset_y + 20.0 + points_size.offset_y,
+            title_text_params.clone(),
         );
 
         next_frame().await;
